8eb007140c45eafbef4692eb4b8201ced5079729
Efrem Dubrovin <efremdubrovin@deltasource.eu>
2021-11-12 11:24:09 +0200

    add separate examples

--- src/main/java/eu/deltasource/SparkOperationsExamples.java
+++ src/main/java/eu/deltasource/SparkOperationsExamples.java
@@ -9,31 +9,30 @@ import java.util.Arrays;
 import java.util.List;
 
 @Slf4j
-public class SparkApplication {
+public class SparkOperationsExamples {
 
 	public static void main(String[] args) {
 		// Initialize Spark
 		SparkConf conf = new SparkConf()
-//			.setJars(new String[]{"target/spark.jar"})
 			.setAppName("test_sum")
-//			.setMaster("spark://spark:7077");
-			.setMaster("local[2]");
+			.setMaster("local[*]");// what local and what is the asterisk?
 		JavaSparkContext sc = new JavaSparkContext(conf);
 
 		// Parallelized collections
 		List<Integer> data = Arrays.asList(1, 2, 3, 4, 5);
 		JavaRDD<Integer> distData = sc.parallelize(data);
 
+		// Reduce operation
 		Integer sum = distData.reduce(Integer::sum);
 		log.info("The sum of elements is: {}", sum);
 
-		// External dataset
-//		JavaRDD<String> lines = sc.textFile("/bitnami/spark/data.txt");
-		JavaRDD<String> lines = sc.textFile("/Users/efrem/Documents/project/spark/src/main/resources/data.txt");
-		// Note data.txt should be accessible from all worker nodes (see mounted volumes)
-		JavaRDD<Integer> lineLengths = lines.map(String::length);
-		int totalLength = lineLengths.reduce(Integer::sum);
+		JavaRDD<Double> squareRoots = distData.map(value -> Math.sqrt(value));
+		squareRoots.foreach(squareRoot -> log.info("root value {}", squareRoot));
 
-		log.info("The sum of all characters in the file: {}", totalLength);
+		// counting the elements in an RDD?
+		long count = squareRoots.count();
+		log.info("Number of elements: {}", count);
+
+		sc.close();
 	}
 }
